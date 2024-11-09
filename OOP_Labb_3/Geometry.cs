ca7790afec3d9cbbd409e08d30dc0d8ca2707994
sidstedt <daniel.sidstedt@gmail.com>
2024-11-09 18:37:38 +0100

    Add project files.

---
+++ OOP_Labb_3/Geometry.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Labb_3
+{
+    public class Geometry
+    {
+        // Virtual method, all shapes have an area
+        // And will be overrided by the derived classes
+        public virtual double Area()
+        {
+            // No implementation, cause each shape calculates differently.
+            return 0;
+        }
+        public virtual double Circumference()
+        {
+            return 0;
+        }
+    }
+    public class Rectangle : Geometry
+    {
+        public double SideA { get; set; }
+        public double SideB { get; set; }
+        public Rectangle()
+        {
+            SideA = 4;
+            SideB = 6;
+        }
+        public Rectangle(double a, double b)
+        {
+            if (a <= 0 || b <= 0)
+            {
+                throw new ArgumentException($"Invalid value {a}. Length and width must be positive");
+            }
+            SideA = a;
+            SideB = b;
+        }
+        // Override the Area method from parent class
+        // gives specific implementation of the rectangles area
+        public override double Area()
+        {
+
+            return SideA * SideB;
+        }
+        public override double Circumference()
+        {
+            return SideA * 2 + SideB * 2;
+        }
+    }
+    public class Square : Geometry
+    {
+        public double Side { get; set; }
+        public Square()
+        {
+            Side = 5;
+        }
+        public Square(double a)
+        {
+            if (a <= 0)
+            {
+                throw new ArgumentException($"The value {a} must pe positive.");
+            }
+            Side = a;
+        }
+        // Override the Area method from parent class
+        // gives specific implementation of the squares area
+        public override double Area()
+        {
+            return Side * Side;
+        }
+        public override double Circumference()
+        {
+            return Side * 4;
+        }
+    }
+    public class Circle : Geometry
+    {
+        public double Radius { get; set; }
+        public Circle()
+        {
+            Radius = 3;
+        }
+        public Circle(double a)
+        {
+            if (a <= 0)
+            {
+                throw new ArgumentException("Invalid value {a}. Radius must be positive number.");
+            }
+            Radius = a;
+        }
+        // Override the Area method from parent class
+        // gives specific implementation of the circles area
+        public override double Area()
+        {
+            return Math.PI * Math.Pow(Radius, 2);
+        }
+        public override double Circumference()
+        {
+            return 2 * Math.PI * Radius;
+        }
+    }
+    public class Triangle : Geometry
+    {
+        public double SideA { get; set; }
+        public double SideB { get; set; }
+        public double SideC { get; set; }
+        public bool IsRightTriangle { get; set; }
+        public Triangle()
+        {
+            SideA = 2.5;
+            SideB = 4;
+            IsRightTriangle = true;
+        }
+        // Construct for general Triangle
+        public Triangle(double a, double b, double c)
+        {
+            if (a <= 0 || b <= 0 || c <= 0)
+            {
+                throw new ArgumentException($"Invalid value {a}, length of any side must be positive");
+            }
+            SideA = a;
+            SideB = b;
+            SideC = c;
+            IsRightTriangle = false;
+        }
+        // Construct for right-angled triangle
+        public Triangle(double a, double b, bool isRightTriangle)
+        {
+            if (a <= 0 || b <= 0)
+            {
+                throw new ArgumentException($"Invalid value {a}. Length of any side must be positive");
+            }
+            SideA = a;
+            SideB = b;
+            IsRightTriangle = isRightTriangle;
+            // Calculate hypotenuse
+            if (isRightTriangle)
+            {
+                SideC = Math.Sqrt(Math.Pow(SideA, 2) + Math.Pow(SideB, 2));
+            }
+        }
+        public override double Area()
+        {
+            double area;
+            if (IsRightTriangle)
+            {
+                return SideA * SideB / 2;
+            }
+            else
+            {
+                // Use Herons formula to calculate area of a general triangle
+                double s = (SideA + SideB + SideC) / 2;
+                return Math.Sqrt(s * (s - SideA) * (s - SideB) * (s - SideC));
+            }
+        }
+        public override double Circumference()
+        {
+            double circumference;
+            if (IsRightTriangle)
+            {
+                // Circumference of right-angled Triangle
+                return SideA + SideB + SideC;
+            }
+            else
+            {
+                // Circumference of general Triangle
+                return SideA + SideB + SideC;
+            }
+        }
+    }
+}
---
+++ OOP_Labb_3/Program.cs
@@ -0,0 +1,115 @@
+﻿namespace OOP_Labb_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Geometry circle = new Circle();
+            //Geometry square = new Square();
+            //Geometry rectangle = new Rectangle();
+
+            //circle.Area();
+            //square.Area();
+            //rectangle.Area();
+
+            // Creating a list based on geometry class
+            List<Geometry> shapes = new List<Geometry>
+            {
+                // Add to the list each child class
+                new Rectangle(),
+                new Square(),
+                new Circle(),
+                // general triangle
+                new Triangle(5.5, 3.2, 7),
+                // right-angled triangle
+                new Triangle(5.5, 3.2, true)
+            };
+            // Iteriate through each shape
+            // Circle, square & rectangle
+            foreach (var shape in shapes)
+            {
+                // Based och the current shape
+                // We will call the correct override method
+                Console.WriteLine(shape.GetType().Name);
+                Console.WriteLine($"Area: {shape.Area()}");
+                Console.WriteLine($"Circumference: {shape.Circumference()}");
+                Console.WriteLine("----------------------");
+            }
+
+            //---- THE MENU ----//
+            bool running = true;
+            while (running)
+            {
+                Console.WriteLine("Välj från menyn vilken form du vill ha och mata sedan in längd/er\n" +
+                    "1. Rectangle\n" +
+                    "2. Square\n" +
+                    "3. Circle\n" +
+                    "4. Rätvinklig Triangel\n" +
+                    "5. Visa sorterad lista\n" +
+                    "6. Exit");
+                int choice;
+                if(!int.TryParse(Console.ReadLine(), out choice))
+                {
+                    Console.WriteLine("Felaktig inmatning, ange ett nummer mellan 1 - 5");
+                    continue;
+                }
+                else
+                {
+                    double length;
+                    double width;
+                    switch (choice)
+                    { 
+                        case 1:
+                            Console.Write("Välj längd: ");
+                            length = double.Parse(Console.ReadLine());
+                            Console.Write("Välj bredd: ");
+                            width = double.Parse(Console.ReadLine());
+                            shapes.Add(new Rectangle(length, width));
+                            break;
+                        case 2:
+                            Console.Write("Välj längd: ");
+                            length = double.Parse(Console.ReadLine());
+                            shapes.Add(new Square(length));
+                            break;
+                        case 3:
+                            Console.Write("Välj radie: ");
+                            double radius = double.Parse(Console.ReadLine());
+                            shapes.Add(new Circle(radius));
+                            break;
+                        case 4:
+                            Console.Write("Välj höjd: ");
+                            length = double.Parse(Console.ReadLine());
+                            Console.Write("Välj bredd: ");
+                            width = double.Parse(Console.ReadLine());
+                            shapes.Add(new Triangle(length, width, true));
+                            break;
+                        case 5:
+                            var sortedShapes = SortShapes(shapes);
+                            foreach (var shape in sortedShapes)
+                            {
+                                Console.WriteLine(shape.GetType().Name);
+                                Console.WriteLine($"Area: {shape.Area()}");
+                                Console.WriteLine($"Circumference: {shape.Circumference()}");
+                                Console.WriteLine("----------------------");
+                            }
+                            break;
+                        case 6:
+                            running = false;
+                            break;
+                        default:
+                            Console.WriteLine("Felaktigt nummer, välj mellan 1 - 5.");
+                            break;
+                    }
+                }
+            }
+
+            Console.ReadKey();
+        }
+        public static List<Geometry> SortShapes(List<Geometry> shapes)
+        {
+            // Use list shapes with method OrderBy, sorting the list
+            // Then create a new list and return it.
+            return shapes.OrderBy(shape => shape.Area()).ToList();
+        }
+    }
+}
